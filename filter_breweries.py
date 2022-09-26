91e82a6ff9f887649a6be39ec3a338dec3cbfe7b
pkwzimm <pzimmerman@epcw.org>
2022-09-25 20:41:23 -0700

    tweaked filter_breweries.py to catch dupliates. Added explanatory comments on both python scripts.

--- filter_breweries.py
+++ filter_breweries.py
@@ -3,27 +3,29 @@ import csv
 
 query = 'United States' # PUT LOCATION YOU CARE ABOUT HERE - can be in the general form of 'Bend, OR UNITED STATES' or 'West Coast New Zealand'
 
-df = pd.read_csv('untappd-1.csv', dtype={'average_rating':float})
-df['num_ratings'] = df['num_ratings'].str.replace(',','')
-df['num_ratings'] = df['num_ratings'].str.replace('M','0000')
-df['num_ratings'] = df['num_ratings'].str.replace('+','')
-df['num_ratings'] = df['num_ratings'].str.replace('.','')
-df['num_ratings'] = pd.to_numeric(df['num_ratings'], errors='coerce')
-df = df[df['num_ratings'].notna()]
+df = pd.read_csv('untappd.csv', dtype={'average_rating':float}) # load datafile from disk
+df = df.drop_duplicates() # drop duplicate rows (for example, headers if you had to run the scraper more than once or stop/start)
+df['num_ratings'] = df['num_ratings'].str.replace(',','') # handle numbers with commas (i.e. 100,000)
+df['num_ratings'] = df['num_ratings'].str.replace('M','0000') # handle Untappd's use of "M+" (i.e. 1.74M+) for number of ratings
+df['num_ratings'] = df['num_ratings'].str.replace('+','') # see above
+df['num_ratings'] = df['num_ratings'].str.replace('.','') # see above
+df['num_ratings'] = pd.to_numeric(df['num_ratings'], errors='coerce') # convert the strings for num_ratings into an int
+df = df[df['num_ratings'].notna()] # no NaNs
 df = df[df['average_rating'].notna()]
 df = df[df['Location'].notna()]
 
 df = df[(df['num_ratings'] > 300)] #filter out very small breweries
 df = df[(df['average_rating'] >= 3.6)] #filter out bad breweries
-df = df[df['Location'].str.contains(query, case=False)]
-df = df.drop_duplicates(subset = ['UT_Index'],keep = 'last').reset_index(drop = True)
+df = df[df['Location'].str.contains(query, case=False)] # run the search on location
+df = df.drop_duplicates(subset = ['UT_Index'],keep = 'last').reset_index(drop = True) # check for dupliates with the same index
+df = df.drop_duplicates(subset = ['UT_URL'],keep = 'last').reset_index(drop = True) # check for dupliates with the same URL
 
 #below is to set up a US-wide list
-df['Location'] = df['Location'].str.replace(' United States', '', case=False)
-df[['City','State']] = df.Location.str.split(', ', expand=True)
+df['Location'] = df['Location'].str.replace(' United States', '', case=False) # remove country from the location, unless that's what you care about.
+df[['City','State']] = df.Location.str.split(', ', expand=True) # Split cities and states into two columns
 df = df[['Brewery_Name','City','State','average_rating','num_ratings','UT_URL']]
 
 df_filename = 'untappd_db.csv'
 print("exporting " + df_filename)
 
-df.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=True)
+df.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=True) # export results file
--- untappd-scraper.py
+++ untappd-scraper.py
@@ -1,22 +1,21 @@
 from selenium import webdriver
-
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC # wait until visible is the key here.
 from selenium.webdriver.chrome.options import Options #options (for running headless)
 import csv
-import re
-import os
-import pandas as pd
+import re # regular expressions
+import os # for checking if file exists
+import pandas as pd # dataframe util
 
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless") # unless you really want to watch the browser window for several hundred thousand fetches...
 browser = webdriver.Chrome(options=options) #open up your browser (Ubuntu version, with headless flags)
 
-df = pd.DataFrame(columns=['UT_Index', 'Brewery_Name', 'Location', 'average_rating', 'num_ratings', 'UT_URL'])
+df = pd.DataFrame(columns=['UT_Index', 'Brewery_Name', 'Location', 'average_rating', 'num_ratings', 'UT_URL']) # create empty df and set column names
 
 # check to make sure you don't delete the datafile if you're ever dumb enough to do so
 if os.path.exists('untappd.csv'):
@@ -27,34 +26,33 @@ print("Opening Untappd...")
 wait = WebDriverWait(browser, 0)
 
 df_filename = 'untappd.csv'
-df.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=False)
+df.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=False) # create datafile (empty except for header at this point)
 
-for i in range(1, 354455):
+for i in range(1, 354455): # untappd has every brewery assigned a number, and this is about as high as they go in 2022.  This will change over time as new breweries are founded.
     brewery_url = 'https://untappd.com/brewery/' + str(i)
 
+    # try-except loop to not fill the csv with empty rows for non-existant brewery numbers (hypothesis is these are no longer active)
     try:
         browser.get(brewery_url)
-        brewery = []
+        brewery = [] # create dict to fill
 
         ut_index = str(i)
 
         brewery_name = wait.until(EC.visibility_of_element_located(
             (By.XPATH, "//div[@class='name']/h1"))).text
 
-        # print('Brewery: ' + brewery_name)
-
         location = wait.until(EC.visibility_of_element_located(
             (By.XPATH, "//div[@class='name']/p[@class='brewery']"))).text
 
         rating = wait.until(EC.visibility_of_element_located(
             (By.XPATH, "//span[@class='num']"))).text
-        rating = re.sub('\(|\)', '', rating)
+        rating = re.sub('\(|\)', '', rating) # remove the parentheses around the average rating
 
         num_ratings = wait.until(EC.visibility_of_element_located(
             (By.XPATH, "//div[@class='stats']/p/span[@class='count']"))).text
 
         ut_url = wait.until(EC.visibility_of_element_located(
-            (By.XPATH, "//a[@class='label']"))).get_attribute("href")
+            (By.XPATH, "//a[@class='label']"))).get_attribute("href") # note that this is an href attribute, the element's text won't get you the URL.
 
         brewery.append([ut_index, brewery_name, location, rating, num_ratings, ut_url])
 
@@ -62,10 +60,11 @@ for i in range(1, 354455):
 
         df_thread = pd.DataFrame(brewery,
                                  columns=['UT_Index', 'Brewery_Name', 'Location', 'average_rating', 'num_ratings',
-                                          'UT_URL'])
+                                          'UT_URL']) # convert dict to a temp df
 
-        df_thread.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=False, mode='a')
+        df_thread.to_csv(df_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL, header=False, mode='a') # append df_thread to csv file
 
+    # this almost always means you get a 404 page for no brewery exists, which means the try fails because there is no element found, so we move onto the next number in the list.
     except:
         print("No brewery at " + brewery_url)
         pass
